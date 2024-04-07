e9a9c44c41ebc79e2bc0c3db13de892a1b2efbc7
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-04-07 23:03:50 +0200

    2 - created District and Region classes.

--- src/main/java/pl/asilver/country_project/subject/District.java
+++ src/main/java/pl/asilver/country_project/subject/District.java
@@ -1,4 +1,8 @@
 package pl.asilver.country_project.subject;
 
+import java.util.List;
+
 public class District {
+    private String name;
+    private List<City> cities;
 }
--- src/main/java/pl/asilver/country_project/subject/Region.java
+++ src/main/java/pl/asilver/country_project/subject/Region.java
@@ -1,4 +1,38 @@
 package pl.asilver.country_project.subject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Region {
+    private String name;
+    private String regionCenter;
+    private double area;
+    private List<District> districts;
+
+    public Region() {
+        this.area = 10000;
+        this.name = name;
+        this.districts = new ArrayList<>();
+        for (int i = 0; i < 2; ++i) {
+            District district = new District();
+            this.districts.add(district);
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getRegionCenter(){
+        return regionCenter;
+    }
+
+    public double getArea(){
+        return area;
+    }
+
+    public List<District> getDistricts(){
+        return districts;
+    }
+
 }
