d89dee921d393ef1e04cb2648ccd880b9f4029dc
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-04-11 22:55:22 +0200

    2 - added constructors, methods to City, District and Region. Created _Main and tested creation of random citizens.

--- src/main/java/pl/asilver/country_project/subject/City.java
+++ src/main/java/pl/asilver/country_project/subject/City.java
@@ -1,4 +1,20 @@
 package pl.asilver.country_project.subject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class City {
+    private List<Citizen> citizens;
+
+    public City(){
+        this.citizens = new ArrayList<>();
+    }
+
+    public List<Citizen> getCitizens() {
+        return citizens;
+    }
+
+    public void addCitizen(Citizen citizen){
+        citizens.add(citizen);
+    }
 }
--- src/main/java/pl/asilver/country_project/subject/District.java
+++ src/main/java/pl/asilver/country_project/subject/District.java
@@ -1,8 +1,20 @@
 package pl.asilver.country_project.subject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class District {
-    private String name;
     private List<City> cities;
+    public District(){
+        this.cities = new ArrayList<>();
+        for (int i = 0; i < 2; ++i){
+            City city = new City();
+            this.cities.add(city);
+        }
+    }
+
+    public List<City> getCities(){
+        return cities;
+    }
+
 }
